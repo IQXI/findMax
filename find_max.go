a9659dd8dfcc97f1b6fd936f4e333a7207e275fa
IQXI <51130504+IQXI@users.noreply.github.com>
2019-06-20 09:35:49 +0300

    Update find_max.go

--- find_max.go
+++ find_max.go
@@ -3,8 +3,11 @@ package main
 import "fmt"
 
 func find_max(slice []interface{}, less func(i, j int) bool) interface{}{
+	if len(slice) == 0{
+		return nil
+	}
 	max := 0
-	for ind, _ := range slice{
+	for ind := range slice{
 		if less(max, ind){
 			max = ind
 		}
@@ -12,23 +15,40 @@ func find_max(slice []interface{}, less func(i, j int) bool) interface{}{
 	return slice[max]
 }
 
+func print(max interface{})  {
+	if max != nil{
+		fmt.Println("Max:", max)
+	}else {
+		fmt.Println("Slice is Empty!")
+	}
+
+}
+
 func main() {
+	//slice of int
 	sl := []interface{}{1, 43, 3, 4}
 	max := find_max(sl, func(i, j int) bool {
 		return sl[i].(int) < sl[j].(int)
 	})
-	fmt.Println("Max:", max)
+	print(max)
 
+	//slice of float
 	sl2 := []interface{}{1.1321, 4.3, 3.4, 4.5}
 	max2 := find_max(sl2, func(i, j int) bool {
 		return sl2[i].(float64) < sl2[j].(float64)
 	})
-	fmt.Println("Max:", max2)
+	print(max2)
 
-	sl3 := []interface{}{"hallow", "my", "friend", "ivan"}
+	//slice of string
+	sl3 := []interface{}{"hallo", "my", "friend", "ivan"}
 	max3 := find_max(sl3, func(i, j int) bool {
 		return sl3[i].(string) < sl3[j].(string)
 	})
-	fmt.Println("Max:", max3)
+	print(max3)
+
+	//empty slice
+	print(find_max([]interface{}{}, func(i, j int) bool {
+		return sl3[i].(string) < sl3[j].(string)
+	}))
 
 }
